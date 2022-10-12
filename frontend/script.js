6e6f063199129718ab0344b05782e52276028cdb
Errorcode07 <bbotond0829@gmail.com>
2022-10-12 12:11:29 +0300

    fetch tested

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,6 @@
-
-let fetchUrl = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+const apiKey = "ARaWUHtGMQKP1vfXovbC8C2H88QwcfEJhkBuJiPW";
+let apiDate = "2007-04-01";
+let fetchUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${apiDate}`;
 
 // fetch(fetchUrl).then(function (response) {
 //     return response.json();
@@ -7,6 +8,28 @@ let fetchUrl = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
 //     console.log(responseJson);
 // })
 
-fetch(fetchUrl)
-    .then(response => response.json())
-    .then(responseJson => console.log(responseJson))
+// fetch(fetchUrl)
+//     .then(response => response.json())
+//     .then(responseJson => console.log(responseJson))
+
+async function fetchNasa () {
+    const response = await fetch(fetchUrl);
+    // console.log(response);
+    const responseJson = await response.json();
+    // console.log(responseJson);
+    return responseJson;
+}
+
+async function loadEvent () {
+    let data = await fetchNasa();
+    console.log("data: ",data);
+
+    const rootElement = document.querySelector("#root");
+    rootElement.insertAdjacentHTML("beforeend",`
+        <h1>${data.title}</h1>
+        <h2>${data.date}</h2>
+        <p>${data.explanation}</p>
+        <img src="${data.url}">
+    `)
+}
+window.addEventListener("load",loadEvent);
